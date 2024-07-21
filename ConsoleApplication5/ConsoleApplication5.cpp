3d5825b6102e2967045581cee868b26eab13d466
Sadiq Quliyev <sadiq000k@gmail.com>
2024-07-22 01:22:58 +0400

    Proje dosyası ekle.

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,155 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+
+   /* cout << "eded daxil edin: ";
+    int t;
+    cin >> t;
+    t % 2 == 0 && cout << "T cut ededdir" || cout << "T tek eddedir";
+    cout << endl;*/
+
+
+
+
+
+
+
+    //cout << "2 eded daxil edin:";
+    //int num2;
+    //cin >> num2;
+    //int num2_2;
+    //cin >> num2_2;
+    //num2 > num2_2&& cout << num2_2 << " daha kicikdir" << endl;
+    //num2_2 > num2&& cout << num2 << " daha kicikdir" << endl;
+    //num2 == num2_2 && cout << "beraberdiler" << endl;
+
+
+
+    //cout << "eded daxil edin: ";
+    //int num;
+    //cin >> num;
+    //num > 0 && cout << "num musbet ededdir:";
+    //num == 0 && cout << "num 0 ededdir:";
+    //num < 0 && cout << "num menfi ededdir:";
+    //cout << endl;
+
+
+
+   /* cout << "2 eded daxil edin: ";
+    double num;
+    cin >> num;
+    double num_2;
+    cin >> num_2;
+
+    cout << "kolkulatorda ne elemek isteyirsen 1.+ \n2.-\n 3.*\n 4./  ";
+    int choise;
+    cin >> choise;
+    if (choise == 1) {
+        double total = num + num_2;
+        cout << total << " cavabdir";
+
+    }
+    else  if (choise == 2) {
+        double total = num - num_2;
+        cout << total << " cavabdir";
+    }
+    else  if (choise == 3) {
+        double total = num * num_2;
+        cout << total << " cavabdir";
+
+    }
+    else  if (choise == 4) {
+        double total = num / num_2;
+        cout << total << " cavabdir";
+
+    }
+    else {
+        cout << "ERROR bele cavab yoxdur";
+    }
+    cout << endl;*/
+
+
+
+
+   /* cout << "eded daxil edin : ";
+    int num;
+    cin >> num;
+    (num > 1 && num < 50) && cout << " eded 1 ve 50 arrasindadir" || cout << "edde 1 50 arrasinda deyil";
+    cout << endl;*/
+
+
+
+
+   /* cout << "2 eded daxil edin :";
+    int num;
+    cin >> num;
+    int num_2;
+    cin >> num_2;
+    num% num_2 == 0 ? cout << "Yes" : cout << "No";
+    cout << endl;*/
+
+
+
+
+
+ /*   cout << "isare daxil edin: ";
+    char c;
+    cin >> c;
+    int num = c;
+    if ((num > 64 && num < 91) || (num > 96 && num < 124))
+        cout << "HERF";
+    else if (num == 46 || num == 33 || num == 63 || num == 44)
+          cout << "PUNKT";
+    cout << endl;*/
+
+
+
+
+   /* cout << "eded daxil edin:";
+    int num;
+    cin >> num;
+    num % 3 == 0 && cout << "eded 3-e bolunur";
+    cout << endl;
+    num % 5 == 0 && cout << "eded 5-e bolunur";
+    cout << endl;
+    num % 7 == 0 && cout << "eded 7-e bolunur";
+    cout << endl;
+*/
+
+
+  /*  cout << "eded daxil edin: ";
+    int num;
+    cin >> num;
+    num < 0 ? num *= -1 : num;
+    cout << "Modulu: " << num << endl;*/
+
+
+
+
+  /*  int num;
+    cin >> num;
+    int say = (num >= 1) + (num >= 10) + (num >= 100) + (num >= 1000);
+    cout << "eded " << say << "  reqemli dir" << endl;
+*/
+
+
+
+    cout << "eded daxil edin: ";
+    int num;
+    cin >> num;
+    int num_1 = num / 1000;
+    int num_2 = num % 10 * 10 + num % 100 / 10;
+    num_1 == num_2 ? cout << "eded polindromdur." : cout << "edded polindrom deyil.";
+    cout << endl;
+
+
+
+
+    
+}
+
